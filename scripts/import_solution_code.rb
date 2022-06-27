33e1dc195d0820a61419124479027eb51ad430ed
Paul Kuruvilla <rohitpaulk@gmail.com>
2022-06-27 21:55:20 +0100

    only import git tracked files

--- scripts/import_solution_code.rb
+++ scripts/import_solution_code.rb
@@ -1,4 +1,5 @@
 # Usage: ruby scripts/import_solution_code.rb <language_slug> <stage_number> <solution_dir>
+# Example: ruby scripts/import_solution_code.rb python 2 /tmp/codecrafters-redis-python
 require_relative "../lib/models"
 
 language_slug = ARGV[0]
@@ -9,6 +10,14 @@ course = Course.load_from_file("../course-definition.yml")
 language = Language.find_by_slug!(language_slug)
 course_stage = course.stages[stage_number - 1]
 
-solution_code_directory = File.join("../solutions", language.slug, course_stage.slug, code)
-`rm -rf #{solution_code_directory}`
-`cp -R #{reference_solution_directory} #{solution_code_directory}`
+solution_code_directory = File.join("../solutions", language.slug, course_stage.slug, "code")
+relative_file_paths = `git -C #{reference_solution_directory} ls-tree -r master --name-only`
+relative_file_paths = relative_file_paths.split("\n").map(&:strip)
+
+relative_file_paths.each do |relative_file_path|
+  reference_file_path = File.join(reference_solution_directory, relative_file_path)
+  solution_file_path = File.join(solution_code_directory, relative_file_path)
+  FileUtils.mkdir_p(File.dirname(solution_file_path))
+  `cp #{reference_file_path} #{solution_file_path}`
+  puts "- imported #{relative_file_path}"
+end
