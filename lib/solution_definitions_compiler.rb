fea331ee793a3ad93c15983490b5e1ad96022a1c
Paul Kuruvilla <rohitpaulk@gmail.com>
2022-07-18 23:23:37 +0100

    Add solution definitions compiler

---
+++ lib/solution_definitions_compiler.rb
@@ -0,0 +1,64 @@
+require "fileutils"
+require "mustache"
+
+require_relative "../lib/models"
+require_relative "../lib/starter_code_uncommenter"
+require_relative "../lib/unindenter"
+
+class SolutionDefinitionsCompiler
+  def initialize(course:, solutions_directory:)
+    @course = course
+    @solutions_directory = solutions_directory
+  end
+
+  def compile_all
+    solution_directories.each do |solution_directory|
+      compile_for_solution_directory(solution_directory)
+    end
+  end
+
+  def compile_for_solution_directory(solution_directory)
+    language = Language.find_by_slug!(File.basename(solution_directory))
+    definition_file_path = File.join(@solutions_directory, language.slug, @course.first_stage.slug, "explanation.md")
+
+    File.delete(definition_file_path) if File.exist?(definition_file_path)
+
+    File.write(definition_file_path, YAML.dump(
+      author_details: {
+        name: 'Paul Kuruvilla',
+        profileUrl: 'https://github.com/rohitpaulk',
+        avatarUrl: 'https://github.com/rohitpaulk.png',
+        headline: 'CTO, CodeCrafters',
+      },
+      reviewers_details: [
+        {
+          name: 'Marcos Lilljedahl',
+          profileUrl: 'https://www.docker.com/captains/marcos-lilljedahl/',
+          avatarUrl: 'https://github.com/marcosnils.png',
+          headline: 'Docker Contributor',
+        }
+      ]
+    ))
+  end
+
+  protected
+
+  def ensure_blocks_exist!(blocks)
+    blocks.each do |block|
+      if block.strip.empty?
+        log_error("Expected uncommenting code to return a block with a marker")
+        log_error("Are you sure there's a contiguous block of comments after the 'Uncomment this' marker?")
+
+        exit(1)
+      end
+
+      # puts ""
+      # puts blocks.inspect
+      # puts ""
+    end
+  end
+
+  def solution_directories
+    Dir.glob(File.join(@solutions_directory, "*", "*"))
+  end
+end
--- scripts/compile_first_stage_solutions.rb
+++ scripts/compile_first_stage_solutions.rb
@@ -1,5 +1,6 @@
 require_relative "../lib/first_stage_solutions_compiler"
 require_relative "../lib/first_stage_explanations_compiler"
+require_relative "../lib/solution_definitions_compiler"
 
 solutions_compiler = FirstStageSolutionsCompiler.new(
   course: Course.load_from_file("../course-definition.yml"),
@@ -13,5 +14,11 @@ explanations_compiler = FirstStageExplanationsCompiler.new(
   solutions_directory: "../solutions"
 )
 
+solution_definitions_compiler = SolutionDefinitionsCompiler.new(
+  course: Course.load_from_file("../course-definition.yml"),
+  solutions_directory: "../solutions"
+)
+
 solutions_compiler.compile_all
 explanations_compiler.compile_all
+solution_definitions_compiler.compile_all
