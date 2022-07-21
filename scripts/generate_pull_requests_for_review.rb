19350d6243cdb6db6840a2d3727dca7b176b61e2
Paul Kuruvilla <rohitpaulk@gmail.com>
2022-07-22 00:55:24 +0100

    first pass at PR gen script

--- scripts/generate_pull_requests_for_review.rb
+++ scripts/generate_pull_requests_for_review.rb
@@ -10,6 +10,7 @@ class PullRequestGenerator
   def generate_all
     solution_directories.each do |solution_directory|
       generate_for_solution_directory(solution_directory)
+      raise "hey"
     end
   end
 
@@ -21,21 +22,66 @@ class PullRequestGenerator
     if YAML.load(solution_definition_contents)["pull_request_url"]
       puts "Skipped #{solution_directory} because it already has a pull request"
     end
-    
+
     language = Language.find_by_slug!(File.basename(File.dirname(solution_directory)))
     stage = @course.stages.detect { |stage| stage.slug.eql?(File.basename(solution_directory)) }
-    definition_file_path = File.join(solution_directory, "definition.yml")
 
+    github_repo_name = "codecrafters-io/#{File.basename(File.dirname(Dir.pwd))}"
 
-    pull_request_url = "abcd"
-    solution_definition_contents.gsub!(/#pull_request_url: .*/, "pull_request_url: #{pull_request_url}")
+    github_client = Octokit::Client.new(access_token: ENV["GITHUB_TOKEN"])
+    gh_repo = github_client.repository(github_repo_name)
+    current_commit_sha = gh_client.commit(gh_repo_name, "main").commit.sha
+    current_tree_sha = gh_client.commit(gh_repo_name, "main").commit.tree.sha
+
+    base_branch_tree = gh_client.create_tree(
+      gh_repo_name,
+      [
+        {
+          path: "solutions/#{language.slug}/#{stage.slug}",
+          sha: null
+        }
+      ],
+      base_tree_sha: current_tree_sha
+    )
+
+    base_commit = gh_client.create_commit(
+      gh_repo,
+      "remove old solution for #{language.slug} - ##{stage.number} (#{stage.name})",
+      base_branch_tree.sha,
+      current_commit_sha
+    )
+
+    head_commit = gh_client.create_commit(
+      gh_repo,
+      "add solution for #{language.slug} - ##{stage.number} (#{stage.name})",
+      current_tree_sha,
+      base_commit.sha
+    )
+
+    base_branch_name = "base-for-#{language.slug}-stage-#{stage.number}"
+    head_branch_name = "add-#{language.slug}-stage-#{stage.number}"
+
+    github_client.create_ref(github_repo_name, "heads/#{base_branch_name}", base_commit.sha)
+    github_client.create_ref(github_repo_name, "heads/#{head_branch_name}", head_commit.sha)
+
+    pull_request = github_client.create_pull_request(
+      github_repo_name,
+      base_branch_name,
+      head_branch_name,
+      "Add solution for #{language.slug} - ##{stage.number} (#{stage.name})",
+      <<~EOF
+        The solution for this stage was created before we setup the reviews feature. You'll notice that this PR doesn't
+        target the `main` branch, this is because the `main` branch already contains the "unreviewed" solution. 
+
+        This PR doesn't have to be merged, it's created for the sole purpose of being reviewed using GitHub's Pull 
+        Request UI. Any changes recommended here will be later merged into the `main` branch.
+      EOF
+    )
+
+    puts "Created pull request: #{pull_request.html_url}"
+
+    solution_definition_contents.gsub!(/#pull_request_url: .*/, "pull_request_url: #{pull_request.html_url}")
     File.write(File.join(solution_directory, "definition.yml"), solution_definition_contents)
-    return
-
-    unless File.exist?(definition_file_path)
-      puts "- Adding commented solution definition for #{@course.slug} (#{language.slug}, #{stage.slug})..."
-      File.write(definition_file_path, COMMENTED_DEFINITION_FILE_CONTENTS)
-    end
   end
 
   def solution_directories
