8280443118e733c4e14e9adb7d0df73e5433ebc4
Paul Kuruvilla <rohitpaulk@gmail.com>
2022-08-17 04:40:15 +0100

    add sync_compiled_starters.rb script

--- lib/repo_syncer.rb
+++ lib/repo_syncer.rb
@@ -80,20 +80,3 @@ class RepoSyncer
     "codecrafters-io/#{File.basename(File.dirname(Dir.pwd))}"
   end
 end
-
-github_client = Octokit::Client.new(access_token: ENV.fetch("GITHUB_TOKEN"))
-
-syncer = RepoSyncer.new(github_client)
-
-Dir["compiled_starters/*"].each do |dir|
-  repo_name = File.basename(dir)
-  puts "Syncing #{repo_name}"
-  pr_url = syncer.sync(repo_name, dir)
-  if pr_url
-    puts "PR: #{pr_url}"
-    `open #{pr_url}` unless ENV["CI"]
-    sleep 5 if ENV["CI"] # Work around PR abuse detected warnings
-  else
-    puts "No changes"
-  end
-end
---
+++ scripts/sync_compiled_starters.rb
@@ -0,0 +1,18 @@
+require_relative "../lib/repo_syncer"
+
+github_client = Octokit::Client.new(access_token: ENV.fetch("GITHUB_TOKEN"))
+
+syncer = RepoSyncer.new(github_client)
+
+Dir["compiled_starters/*"].each do |dir|
+  repo_name = File.basename(dir)
+  puts "Syncing #{repo_name}"
+  pr_url = syncer.sync(repo_name, dir)
+  if pr_url
+    puts "PR: #{pr_url}"
+    `open #{pr_url}` unless ENV["CI"]
+    sleep 5 if ENV["CI"] # Work around PR abuse detected warnings
+  else
+    puts "No changes"
+  end
+end
