4d14af59fd718e63e47e99196a26a8c079df7261
Paul Kuruvilla <rohitpaulk@gmail.com>
2022-09-08 18:06:34 +0100

    Add test for missing dependency files

--- lib/dockerfile_tester.rb
+++ lib/dockerfile_tester.rb
@@ -1,5 +1,6 @@
 require_relative "logger"
 require_relative "test_harness"
+require_relative "dockerfile"
 
 require "open3"
 
@@ -18,10 +19,6 @@ class DockerfileTester < TestHarness
     new(course, language_pack)
   end
 
-  def copied_starter_dir
-    @copied_starter_dir ||= Dir.mktmpdir.tap { |x| FileUtils.rmdir(x) }
-  end
-
   def language
     return Language.find_by_slug!("javascript") if language_pack.start_with?("nodejs")
     return Language.find_by_slug!("csharp") if language_pack.start_with?("dotnet")
@@ -29,7 +26,7 @@ class DockerfileTester < TestHarness
   end
 
   def do_test
-    FileUtils.cp_r(starter_dir, copied_starter_dir)
+    copy_starter_dir!
 
     log_header("Testing Dockerfile: #{slug}")
 
@@ -43,7 +40,24 @@ class DockerfileTester < TestHarness
     time_taken = assert_time_under(5) { build_image }
 
     log_success "Took #{time_taken} secs"
+
+    if dockerfile.dependency_file_paths.any?
+      copy_starter_dir!
+
+      dockerfile.dependency_file_paths.each do |dependency_file_path|
+        log_info ""
+        log_info "Building #{language_pack} with missing #{dependency_file_path} file"
+
+        FileUtils.rm_rf("#{copied_starter_dir}/#{dependency_file_path}")
+        time_taken = assert_time_under(300) { build_image }
+
+        log_success "Took #{time_taken} secs"
+        log_info ""
+      end
+    end
   end
+
+  protected
 
   def build_image
     assert_stdout_contains(
@@ -52,12 +66,25 @@ class DockerfileTester < TestHarness
     )
   end
 
-  def slug
-    "#{course.slug}-#{language_pack}"
+  def copy_starter_dir!
+    @copied_starter_dir = nil
+    FileUtils.cp_r(starter_dir, copied_starter_dir)
+  end
+
+  def copied_starter_dir
+    @copied_starter_dir ||= Dir.mktmpdir.tap { |x| FileUtils.rmdir(x) }
+  end
+
+  def dockerfile
+    @dockerfile ||= Dockerfile.parse_from_file(dockerfile_path)
   end
 
   def dockerfile_path
     "../dockerfiles/#{language_pack}.Dockerfile"
+  end
+
+  def slug
+    "#{course.slug}-#{language_pack}"
   end
 
   def starter_dir
