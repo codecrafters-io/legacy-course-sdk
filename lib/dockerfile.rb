db2005685821b31e6f94cc9c47c6cafe7630fea2
Paul Kuruvilla <rohitpaulk@gmail.com>
2022-09-09 18:23:24 +0100

    Revert "Add test for missing dependency files"
    This reverts commit 4d14af59fd718e63e47e99196a26a8c079df7261.

--- lib/dockerfile.rb
+++
@@ -1,8 +0,0 @@
-class Dockerfile < Struct.new(:dependency_file_paths, keyword_init: true)
-  def self.parse_from_file(file_path)
-    dockerfile_contents = File.read(file_path)
-    dependency_line_matches = dockerfile_contents.match(/CODECRAFTERS_DEPENDENCY_FILE_PATHS="([^"]*)"/)
-
-    new(dependency_file_paths: dependency_line_matches ? dependency_line_matches[1].split(",") : [])
-  end
-end
--- lib/dockerfile_tester.rb
+++ lib/dockerfile_tester.rb
@@ -1,6 +1,5 @@
 require_relative "logger"
 require_relative "test_harness"
-require_relative "dockerfile"
 
 require "open3"
 
@@ -19,6 +18,10 @@ class DockerfileTester < TestHarness
     new(course, language_pack)
   end
 
+  def copied_starter_dir
+    @copied_starter_dir ||= Dir.mktmpdir.tap { |x| FileUtils.rmdir(x) }
+  end
+
   def language
     return Language.find_by_slug!("javascript") if language_pack.start_with?("nodejs")
     return Language.find_by_slug!("csharp") if language_pack.start_with?("dotnet")
@@ -26,7 +29,7 @@ class DockerfileTester < TestHarness
   end
 
   def do_test
-    copy_starter_dir!
+    FileUtils.cp_r(starter_dir, copied_starter_dir)
 
     log_header("Testing Dockerfile: #{slug}")
 
@@ -40,24 +43,7 @@ class DockerfileTester < TestHarness
     time_taken = assert_time_under(5) { build_image }
 
     log_success "Took #{time_taken} secs"
-
-    if dockerfile.dependency_file_paths.any?
-      copy_starter_dir!
-
-      dockerfile.dependency_file_paths.each do |dependency_file_path|
-        log_info ""
-        log_info "Building #{language_pack} with missing #{dependency_file_path} file"
-
-        FileUtils.rm_rf("#{copied_starter_dir}/#{dependency_file_path}")
-        time_taken = assert_time_under(300) { build_image }
-
-        log_success "Took #{time_taken} secs"
-        log_info ""
-      end
-    end
   end
-
-  protected
 
   def build_image
     assert_stdout_contains(
@@ -66,25 +52,12 @@ class DockerfileTester < TestHarness
     )
   end
 
-  def copy_starter_dir!
-    @copied_starter_dir = nil
-    FileUtils.cp_r(starter_dir, copied_starter_dir)
-  end
-
-  def copied_starter_dir
-    @copied_starter_dir ||= Dir.mktmpdir.tap { |x| FileUtils.rmdir(x) }
-  end
-
-  def dockerfile
-    @dockerfile ||= Dockerfile.parse_from_file(dockerfile_path)
+  def slug
+    "#{course.slug}-#{language_pack}"
   end
 
   def dockerfile_path
     "../dockerfiles/#{language_pack}.Dockerfile"
-  end
-
-  def slug
-    "#{course.slug}-#{language_pack}"
   end
 
   def starter_dir
