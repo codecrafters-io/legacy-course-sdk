e8e2a321590c9962836f66dd9a4574a4d785c2c6
Paul Kuruvilla <rohitpaulk@gmail.com>
2022-09-30 19:58:32 +0100

    remove individual compile scripts

--- scripts/compile_solution_definitions.rb
+++
@@ -1,9 +0,0 @@
-require_relative "../lib/solution_definitions_compiler"
-require_relative "../lib/models"
-
-solution_definitions_compiler = SolutionDefinitionsCompiler.new(
-  course: Course.load_from_file("../course-definition.yml"),
-  solutions_directory: "../solutions"
-)
-
-solution_definitions_compiler.compile_all
