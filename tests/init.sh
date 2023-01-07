e7002fe085244e86f8fb7ecb3cd7e098843f1286
Paul Kuruvilla <rohitpaulk@gmail.com>
2023-01-07 12:25:57 +0000

    fix app-cached copying code

--- tests/init.sh
+++ tests/init.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 set -e
 
-test -d /app-cached && mv /app-cached/* /app
+if test -d /app-cached; then
+  for x in /app-cached/* /app-cached/.[!.]* /app-cached/..?*; do
+    if [ -e "$x" ]; then mv -- "$x" /app/; fi
+  done
+fi
+
 test -f /codecrafters-precompile.sh && /bin/sh /codecrafters-precompile.sh > /dev/null
 
 exec /tester/test.sh
