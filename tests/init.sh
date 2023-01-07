6e7b8c236e9e71e80783237d59fffc5148998d53
Paul Kuruvilla <rohitpaulk@gmail.com>
2023-01-07 12:50:56 +0000

    change app-cached handling

--- tests/init.sh
+++ tests/init.sh
@@ -1,12 +1,7 @@
 #!/bin/sh
 set -e
 
-if test -d /app-cached; then
-  for x in /app-cached/* /app-cached/.[!.]* /app-cached/..?*; do
-    if [ -e "$x" ]; then mv -- "$x" /app/; fi
-  done
-fi
-
+test -d /app-cached && cp -R /app-cached/. /app
 test -f /codecrafters-precompile.sh && /bin/sh /codecrafters-precompile.sh > /dev/null
 
 exec /tester/test.sh
