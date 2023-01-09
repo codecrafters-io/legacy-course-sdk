6aec5a0db551685b67b9d9eda06e802c3ed9fb21
Paul Kuruvilla <rohitpaulk@gmail.com>
2023-01-09 20:21:13 +0000

    don't modify mtimes

--- tests/init.sh
+++ tests/init.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 
-test -d /app-cached && cp -R /app-cached/. /app
+test -d /app-cached && cp -p -R /app-cached/. /app
 test -f /codecrafters-precompile.sh && /bin/sh /codecrafters-precompile.sh > /dev/null
 
 exec /tester/test.sh
