cf31752abdcb19e116022e52359e16a00a8c2ba4
Paul Kuruvilla <rohitpaulk@gmail.com>
2023-01-30 23:10:23 +0000

    update tester logic

--- tests/init.sh
+++ tests/init.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 set -e
 
-test -d /app-cached && cp -p -R /app-cached/. /app
+rm -rf /tmp/app
+cp -r /app /tmp/app
+cd /tmp/app
+
+CODECRAFTERS_SUBMISSION_DIR=/tmp/app
+
+test -d /app-cached && cp -p -R /app-cached/. "$CODECRAFTERS_SUBMISSION_DIR"
 test -f /codecrafters-precompile.sh && /bin/sh /codecrafters-precompile.sh > /dev/null
 
 exec /tester/test.sh
