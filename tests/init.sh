2b49b267802f687c7989c5d81a130f52fb92e85b
Paul Kuruvilla <rohitpaulk@gmail.com>
2023-11-02 16:18:44 +0000

    enable compilation logs

--- tests/init.sh
+++ tests/init.sh
@@ -4,6 +4,6 @@ set -e
 CODECRAFTERS_SUBMISSION_DIR=/app
 
 test -d /app-cached && cp -p -R /app-cached/. "$CODECRAFTERS_SUBMISSION_DIR"
-test -f /codecrafters-precompile.sh && /bin/sh /codecrafters-precompile.sh >/dev/null
+test -f /codecrafters-precompile.sh && /bin/sh /codecrafters-precompile.sh | sed 's/^/[compile] /'
 
 exec /tester/test.sh
